f25abef087e591424cca3fd5a9ff3d77cc61822d
sajdkabir <sajda.kbir@gmail.com>
2024-07-03 13:59:23 +0530

    feat: active nav up

--- project2/app/NavBar.tsx
+++ project2/app/NavBar.tsx
@@ -1,8 +1,14 @@
+'use client';
+
 import Link from 'next/link';
 import React from 'react';
 import { FaBug } from "react-icons/fa";
+import { usePathname } from 'next/navigation'
+import classNames from 'classnames'
 
 const NavBar = () => {
+    const currentPath= usePathname()
+
     const links= [
         {label: 'Dashboard', href:'/'},
         {label: 'Issues', href:'/issues'},
@@ -14,7 +20,14 @@ const NavBar = () => {
         {/* <Link href="/"><PiBugThin /></Link> */}
         <Link href="/"><FaBug /></Link>
         <ul className='flex space-x-6'>
-            {links.map(link =><Link key={link.href} className='text-zinc-500 hover:text-zinc-800 transition-colors'
+            {links.map(link =><Link key={link.href} className={
+                classNames({
+                    'text-zinc-900': link.href===currentPath,
+                    'text-zinc-500': link.href!==currentPath,
+                    'hover:text-zinc-800 transition-colors': true
+                }
+                )
+            }
              href={link.href} >{link.label}</Link>)}
 
         </ul>
